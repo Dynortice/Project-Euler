fce64db7f0347b701b789baa25a67f4cc48dd505
Vladyslav Harmash <vladyslav.harmash@trinetix.com>
2020-09-09 16:22:35 +0300

    Problem 9

---
+++ problem_0009/compute.jl
@@ -0,0 +1,13 @@
+function compute(n::Integer)::Integer
+    for c in n ÷ 3 + 1:n ÷ 2
+        s = c * c - n * n + 2 * c * n
+        if s > 0
+            t = trunc(Int, √s)
+            if t * t ≡ s
+                b = (n - c + t) / 2
+                a = n - c - b
+                return a * b * c
+            end
+        end
+    end
+end
